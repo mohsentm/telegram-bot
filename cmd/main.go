1d0c0a6720d094a18f357406563222cc6feab8a5
Mohsen Hosseini <hosseini.m1370@gmail.com>
2020-03-31 03:33:48 +0300

    fixed the load env

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"github.com/mohsentm/telegram-bot/config"
 	"github.com/mohsentm/telegram-bot/internal/telegramBot"
 )
-
-func init() {
-	config.Init()
-}
 
 func main() {
 	telegramBot.WakeUp()
